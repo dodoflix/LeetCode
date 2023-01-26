95f7b6bccf31779a403d2fc6e4135b2806ddadec
dodoflix <dogukanmetan@gmail.com>
2023-01-26 05:42:34 +0300

    Folder updates

--- src/Program.cs
+++ src/Program.cs
@@ -2,4 +2,4 @@
 
 var problems = new Problems();
 Console.Write("The problem ID to solve:");
-Console.WriteLine(problems.Solve(Convert.ToInt32(Console.ReadLine())));
+//Console.WriteLine(problems.Solve(Convert.ToInt32(Console.ReadLine())));
